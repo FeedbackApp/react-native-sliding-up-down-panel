b84f8820eb1b630f57162e41d16fa26690e3fbf0
Dev Maharaj <dev@feedbackapp.ca>
2018-07-31 15:57:02 -0400

    test

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ export default class SlidingPanel extends Component {
         }
 
         console.log('@onPanResponderRelease : ' + a);
-        if(a > 2){
+        if(a >= 0s){
           console.log('supposed to be going up ')
           sliderPosition = this.props.maxDragHeight-this.props.headerLayoutHeight
           this.props.onAnimationStart();
